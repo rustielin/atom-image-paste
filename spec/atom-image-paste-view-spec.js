5e4c36ce5d96c04c05dfe5c4c18985c32c8beb83
rustielin <rustielin@berkeley.edu>
2018-05-15 00:09:57 -0700

    initial commit

---
+++ spec/atom-image-paste-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomImagePasteView from '../lib/atom-image-paste-view';
+
+describe('AtomImagePasteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
